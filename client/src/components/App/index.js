3da06197412e5426dafde153ee83423af648a532
Adel Pazoki <apazokit@uwaterloo.ca>
2022-05-18 23:24:08 -0400

    first commit for lab 3

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,43 +1,8 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
-
-import Home from '../Home';
-import PrivateRoute from '../Navigation/PrivateRoute.js';
-
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
-
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-
-  render() {
-    return (
-	  <Router>
-	    <div>
-        <PrivateRoute exact path="/" component={Home}/>
-	    </div>
-	  </Router>
-    );
-  }
+import * as React from 'react';
+function App() {
+return (
+<div>
+<h1>Hello World</h1>
+</div> );
 }
-
 export default App;

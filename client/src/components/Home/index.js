54dd1f5808d17b6144030fbbb95cdd083948556c
Adel Pazoki <apazokit@uwaterloo.ca>
2022-06-17 00:19:04 -0400

    Finished Deliverable 1

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import ReviewsIcon from "@material-ui/icons/RateReview";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from "@material-ui/core/FormLabel";
+import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+import MovieIcon from "@material-ui/icons/Movie";
+import { ToastContainer, toast, Bounce } from "material-react-toastify";
+import "material-react-toastify/dist/ReactToastify.css";
 
 //Dev mode
 const serverURL = ""; //enable for dev mode
 
 //Deployment mode instructions
 //const serverURL = "http://ov-research-4.uwaterloo.ca:PORT"; //enable for deployed mode; Change PORT to the port number given to you;
-//To find your port number: 
-//ssh to ov-research-4.uwaterloo.ca and run the following command: 
+//To find your port number:
+//ssh to ov-research-4.uwaterloo.ca and run the following command:
 //env | grep "PORT"
 //copy the number only and paste it in the serverURL in place of PORT, e.g.: const serverURL = "http://ov-research-4.uwaterloo.ca:3000";
 
@@ -24,20 +41,20 @@ const opacityValue = 0.9;
 
 const theme = createTheme({
   palette: {
-    type: 'dark',
+    type: "light",
     background: {
-      default: "#000000"
+      default: "White",
     },
     primary: {
-      main: "#52f1ff",
+      main: "#FF8C00",
     },
     secondary: {
-      main: "#b552f7",
+      main: "#000000",
     },
   },
 });
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     body: {
       backgroundColor: "#000000",
@@ -52,7 +69,7 @@ const styles = theme => ({
   mainMessageContainer: {
     marginTop: "20vh",
     marginLeft: theme.spacing(20),
-    [theme.breakpoints.down('xs')]: {
+    [theme.breakpoints.down("xs")]: {
       marginLeft: theme.spacing(4),
     },
   },
@@ -64,32 +81,59 @@ const styles = theme => ({
     maxWidth: 250,
     paddingBottom: theme.spacing(2),
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  },
 
+  RateSection: {
+    margin: theme.spacing(1),
+    position: "relative",
+    top: "10px",
+  },
+
+  TextField: {
+    margin: theme.spacing(1),
+    minWidth: 200,
+  },
+
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+
+  button: {
+    margin: theme.spacing(1),
+    fontWeight: "bolder",
+    position: "relative",
+    top: "20px",
+  },
+
+  title: {
+    margin: theme.spacing(1),
+    fontWeight: "bolder",
+  },
 });
-
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userID: 1,
-      mode: 0
-    }
-  };
+      mode: 0,
+    };
+  }
 
   componentDidMount() {
     //this.loadUserSettings();
   }
 
-
   loadUserSettings() {
-    this.callApiLoadUserSettings()
-      .then(res => {
-        //console.log("loadUserSettings returned: ", res)
-        var parsed = JSON.parse(res.express);
-        console.log("loadUserSettings parsed: ", parsed[0].mode)
-        this.setState({ mode: parsed[0].mode });
-      });
+    this.callApiLoadUserSettings().then((res) => {
+      //console.log("loadUserSettings returned: ", res)
+      var parsed = JSON.parse(res.express);
+      console.log("loadUserSettings parsed: ", parsed[0].mode);
+      this.setState({ mode: parsed[0].mode });
+    });
   }
 
   callApiLoadUserSettings = async () => {
@@ -102,19 +146,17 @@ class Home extends Component {
         //authorization: `Bearer ${this.state.token}`
       },
       body: JSON.stringify({
-        userID: this.state.userID
-      })
+        userID: this.state.userID,
+      }),
     });
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     console.log("User settings: ", body);
     return body;
-  }
+  };
 
   render() {
     const { classes } = this.props;
-
-
 
     const mainMessage = (
       <Grid
@@ -123,50 +165,313 @@ class Home extends Component {
         direction="column"
         justify="flex-start"
         alignItems="flex-start"
-        style={{ minHeight: '100vh' }}
+        style={{ minHeight: "100vh" }}
         className={classes.mainMessageContainer}
       >
         <Grid item>
-
           <Typography
             variant={"h3"}
             className={classes.mainMessage}
             align="flex-start"
           >
             {this.state.mode === 0 ? (
-              <React.Fragment>
-                Welcome to MSci245!
-              </React.Fragment>
+              <React.Fragment>Welcome to MSci245!</React.Fragment>
             ) : (
-              <React.Fragment>
-                Welcome back!
-              </React.Fragment>
+              <React.Fragment>Welcome back!</React.Fragment>
             )}
           </Typography>
-
         </Grid>
       </Grid>
-    )
-
+    );
 
     return (
       <MuiThemeProvider theme={theme}>
         <div className={classes.root}>
           <CssBaseline />
-          <Paper
-            className={classes.paper}
-          >
-            {mainMessage}
-          </Paper>
-
         </div>
+
+        <Review />
       </MuiThemeProvider>
     );
   }
 }
-
 Home.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Home);
+
+const Review = (props) => {
+  const notify = () => toast.error("🎥Please enter your review title");
+  const notify2 = () => toast.error("🎥Please enter your review");
+  const notify3 = () => toast.error("🎥Please enter your rating");
+  const notifyAll = () =>
+    toast.success(
+      <p>
+        🎉your data was received successfully
+        <br />
+        <br />
+        🎥selected movie: {selectedMovie}
+        <br />
+        <br />
+        🎥review title: {reviewTitleValue}
+        <br />
+        <br />
+        🎥review body: {reviewValue}
+        <br />
+        <br />
+        ⭐️review rating: {ratingValue}
+      </p>,
+      {
+        autoClose: 5000,
+      }
+    );
+  // var error = false
+  const [reviewTitleValue, setReviewTitleValue] = React.useState("");
+  const [error, setError] = React.useState(false);
+  const [reviewValue, setReviewValue] = React.useState("");
+  const [errorReview, setErrorReview] = React.useState(false);
+  const [ratingValue, setRatingValue] = React.useState("");
+  const [selectedMovie, setSelectedMovie] = React.useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (
+      reviewTitleValue.length > 0 &&
+      reviewValue.length > 0 &&
+      ratingValue.length > 0
+    ) {
+      notifyAll();
+    }
+    if (reviewTitleValue.length === 0) {
+      setError(true);
+      notify();
+    }
+
+    if (ratingValue == "") {
+      notify3();
+    }
+
+    if (reviewTitleValue.length !== 0) {
+      setError(false);
+    }
+    if (reviewValue.length !== 0) {
+      setErrorReview(false);
+    }
+
+    if (reviewValue.length === 0) {
+      setErrorReview(true);
+      notify2();
+    }
+    setReviewTitleValue("");
+    setReviewValue("");
+    setRatingValue("");
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        <Typography
+          style={{ margin: theme.spacing(1), fontWeight: "bolder" }}
+          variant="h3"
+          gutterBottom={true}
+        >
+          Movie Review Website
+        </Typography>
+      </Grid>
+      <MovieSelection onChange={setSelectedMovie} value={selectedMovie || ""} />
+      <ReviewTitle
+        onChange={setReviewTitleValue}
+        error={error}
+        value={reviewTitleValue}
+      />
+      <ReviewBody
+        onChange={setReviewValue}
+        error={errorReview}
+        value={reviewValue}
+      />
+      <ReviewRating onChange={setRatingValue} value={ratingValue} />
+      <Button
+        variant="contained"
+        color="primary"
+        style={{
+          margin: theme.spacing(1),
+          fontWeight: "bolder",
+          position: "relative",
+          top: "20px",
+        }}
+        endIcon={<MovieIcon />}
+        onClick={submitHandler}
+      >
+        Submit
+      </Button>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        transition={Bounce}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+};
+
+const MovieSelection = (props) => {
+  const SelectedMovieHandler = (event) => {
+    props.onChange(event.target.value || null);
+  };
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      alignItems="flex-start"
+    >
+      <FormControl style={{ margin: theme.spacing(1), minWidth: 200 }}>
+        <InputLabel>Title</InputLabel>
+        <Select onChange={SelectedMovieHandler} value={props.value}>
+          <MenuItem value={"There Will Be Blood"}>There Will Be Blood</MenuItem>
+          <MenuItem value={"Inception"}>Inception</MenuItem>
+          <MenuItem value={"Soul"}>Soul</MenuItem>
+          <MenuItem value={"Batman"}>Batman</MenuItem>
+          <MenuItem value={"LaLa Land"}>LaLa Land</MenuItem>
+        </Select>
+        <FormHelperText>Select a Movie</FormHelperText>
+      </FormControl>
+    </Grid>
+  );
+};
+
+const ReviewTitle = (props) => {
+  const ReviewTitleHandler = (event) => {
+    props.onChange(event.target.value);
+  };
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      alignItems="flex-start"
+    >
+      <TextField
+        style={{ margin: theme.spacing(1), minWidth: 200 }}
+        id="standard-secondary"
+        label="Title"
+        // helperText ={props.value}
+        helperText="Write the Title of the Movie"
+        color="primary"
+        error={props.error}
+        onChange={ReviewTitleHandler}
+        value={props.value}
+      />
+    </Grid>
+  );
+};
+
+const ReviewBody = (props) => {
+  const ReviewHandler = (event) => {
+    props.onChange(event.target.value);
+  };
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      alignItems="flex-start"
+    >
+      <TextField
+        style={{ margin: theme.spacing(1), minWidth: 200 }}
+        id="standard-textarea"
+        placeholder="Review"
+        helperText="Write Your Review"
+        onChange={ReviewHandler}
+        value={props.value}
+        error={props.error}
+        multiline
+        minRows={5}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <ReviewsIcon style={{ position: "relative", top: "-35px" }} />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </Grid>
+  );
+};
+
+const ReviewRating = (props) => {
+  const ReviewRatingHandler = (event) => {
+    props.onChange(event.target.value);
+  };
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      alignItems="flex-start"
+    >
+      <FormControl
+        style={{ margin: theme.spacing(1), position: "relative", top: "10px" }}
+      >
+        <FormLabel component="legend">Rate The Movie</FormLabel>
+        <RadioGroup
+          row
+          aria-label="position"
+          name="Rating"
+          component="fieldset"
+          onChange={ReviewRatingHandler}
+          value={props.value}
+        >
+          <FormControlLabel
+            value="1"
+            control={<Radio color="primary" />}
+            label="1"
+            labelPlacement="bottom"
+          />
+          <FormControlLabel
+            value="2"
+            control={<Radio color="primary" />}
+            label="2"
+            labelPlacement="bottom"
+          />
+          <FormControlLabel
+            value="3"
+            control={<Radio color="primary" />}
+            label="3"
+            labelPlacement="bottom"
+          />
+          <FormControlLabel
+            value="4"
+            control={<Radio color="primary" />}
+            label="4"
+            labelPlacement="bottom"
+          />
+          <FormControlLabel
+            value="5"
+            control={<Radio color="primary" />}
+            label="5"
+            labelPlacement="bottom"
+          />
+        </RadioGroup>
+      </FormControl>
+    </Grid>
+  );
+};
